3a13d7fe4b04cbc8a249b05bd9fe613a3f85a230
hp <shivamsharma220827@acropolis.in>
2026-08-09 23:33:21 +0530

    this is my commit

---
+++ linkedlist/LinkedList.java
@@ -0,0 +1,34 @@
+class LinkedList {
+    // node class implementation
+static class Node{
+int data;
+Node next;
+
+Node(int data){
+this.data=data;
+this.next=null;
+}
+}
+
+// some node creation
+
+public static void main(String args[]){
+    Node first=new Node(10);
+    Node second=new Node(20);
+    Node third=new Node(30);
+    Node fourth=new Node(40);
+    Node fifth=new Node(50);
+    Node sixth=new Node(60);
+    first.next=second;
+    second.next=third;
+    third.next=fourth;
+    fourth.next=fifth;
+    fifth.next=sixth;
+
+   while(first!=null){
+       System.out.println(first.data);
+       first=first.next;
+   }
+}
+
+}
