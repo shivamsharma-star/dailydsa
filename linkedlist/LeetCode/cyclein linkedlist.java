151252009e18552f89c8f755443ff811cbfb9bbb
hp <shivamsharma220827@acropolis.in>
2026-08-15 00:15:07 +0530

    cycle in ;ikedlist

--- linkedlist/LeetCode/cyclein linkedlist.java
+++ linkedlist/LeetCode/cyclein linkedlist.java
@@ -1,16 +1,13 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
 
-        if (head == null || head.next == null) {
-            return false;
-        }
-
-        ListNode pt1 = head;      // slow
-        ListNode pt2 = head;      // fast
+        ListNode pt1 = head;   // slow
+        ListNode pt2 = head;   // fast
 
         while (pt2 != null && pt2.next != null) {
-            pt1 = pt1.next;           // move 1 step
-            pt2 = pt2.next.next;      // move 2 steps
+
+            pt1 = pt1.next;          // 1 step
+            pt2 = pt2.next.next;     // 2 steps
 
             if (pt1 == pt2) {
                 return true;
