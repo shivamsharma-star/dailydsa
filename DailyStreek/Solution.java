46735b357ea6f16298e7edd1213a789356a0eaeb
hp <shivamsharma220827@acropolis.in>
2026-08-17 23:25:56 +0530

    g

---
+++ DailyStreek/Solution.java
@@ -0,0 +1,61 @@
+public class Solution {
+    public int stoneGameV(int[] stoneValue) {
+
+        int n = stoneValue.length;
+
+        // Prefix sum
+        int[] prefix = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + stoneValue[i];
+        }
+
+        // dp[i][j] = maximum score Alice can get from i to j
+        int[][] dp = new int[n][n];
+
+        // length = 2 se start
+        for (int len = 2; len <= n; len++) {
+
+            for (int i = 0; i + len <= n; i++) {
+
+                int j = i + len - 1;
+
+                // Try every possible split
+                for (int k = i; k < j; k++) {
+
+                    int leftSum = prefix[k + 1] - prefix[i];
+                    int rightSum = prefix[j + 1] - prefix[k + 1];
+
+                    if (leftSum < rightSum) {
+
+                        dp[i][j] = Math.max(
+                            dp[i][j],
+                            leftSum + dp[i][k]
+                        );
+
+                    } else if (leftSum > rightSum) {
+
+                        dp[i][j] = Math.max(
+                            dp[i][j],
+                            rightSum + dp[k + 1][j]
+                        );
+
+                    } else {
+
+                        dp[i][j] = Math.max(
+                            dp[i][j],
+                            leftSum + Math.max(
+                                dp[i][k],
+                                dp[k + 1][j]
+                            )
+                        );
+                    }
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+} {
+    
+}
