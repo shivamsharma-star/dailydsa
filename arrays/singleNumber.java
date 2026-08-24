1d9b2639cb091c733a7301b62f5ef855671c057b
hp <shivamsharma220827@acropolis.in>
2026-08-24 17:49:26 +0530

    this is single element problem

---
+++ arrays/singleNumber.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> mySet = new HashSet<>();
+
+        for (int num : nums) {
+            if (mySet.contains(num)) {
+                mySet.remove(num);
+            } else {
+                mySet.add(num);
+            }
+        }
+
+        return mySet.iterator().next();
+    }
+}
