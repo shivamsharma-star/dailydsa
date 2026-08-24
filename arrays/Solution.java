fb22adb8366df2e726eff405a7d5e7bdd3e5c9f4
hp <shivamsharma220827@acropolis.in>
2026-08-24 17:51:44 +0530

    this is single element problem

---
+++ arrays/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+
+        for (int num : nums) {
+            ans ^= num;
+        }
+
+        return ans;
+    }
+} {
+    
+}
