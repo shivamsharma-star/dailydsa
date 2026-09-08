ba48a8ddc8ac7b331c2447181e557c9ee44713ad
hp <shivamsharma220827@acropolis.in>
2026-09-08 18:43:31 +0530

    queue using stack

---
+++ StackAndQueue/QueueUsingStack.java
@@ -0,0 +1,39 @@
+class MyQueue {
+
+    Stack<Integer> mystack1 = new Stack<>();
+    Stack<Integer> mystack2 = new Stack<>();
+
+    public MyQueue() {
+        
+    }
+
+    public void push(int x) {
+        mystack1.push(x);
+    }
+
+    public int pop() {
+
+        if (mystack2.isEmpty()) {
+            while (!mystack1.isEmpty()) {
+                mystack2.push(mystack1.pop());
+            }
+        }
+
+        return mystack2.pop();
+    }
+
+    public int peek() {
+
+        if (mystack2.isEmpty()) {
+            while (!mystack1.isEmpty()) {
+                mystack2.push(mystack1.pop());
+            }
+        }
+
+        return mystack2.peek();
+    }
+
+    public boolean empty() {
+        return mystack1.isEmpty() && mystack2.isEmpty();
+    }
+}
