5b5c18f4f269415b63cab7ff6b2cf98caa475365
hp <shivamsharma220827@acropolis.in>
2026-09-13 18:15:17 +0530

    daily streek2

--- DailyStreek/largestoverlap.java
+++ DailyStreek/largestoverlap.java
@@ -12,7 +12,7 @@ class Solution {
 
                 int count = 0;
 
-                // Check every cell of img1
+                
                 for (int i = 0; i < n; i++) {
                     for (int j = 0; j < n; j++) {
 
