3c920e8b35610215bd160a071df09c39e39fcacf
hp <shivamsharma220827@acropolis.in>
2026-09-16 16:49:46 +0530

    final

--- DailyStreek/numberOfSets.java
+++ DailyStreek/numberOfSets.java
@@ -1,11 +1,12 @@
 class Solution {
+    // function to calculate the maximum number of non-overlapping palindromic substrings of length at least k
     public int numberOfSets(int n, int k) {
 //  long MOD = 1000000007L;
         long MOD = 1000000007L;
 
         long[] dp = new long[2 * k + 1];
         dp[0] = 1;
-
+//  for each point from 1 to n + k - 1, update the dp array to count the number of ways to choose segments
         for (int i = 1; i <= n + k - 1; i++) {
 
             for (int j = Math.min(i, 2 * k); j >= 1; j--) {
