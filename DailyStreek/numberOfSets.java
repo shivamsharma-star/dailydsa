358c6c279f851b436ce4b4975f2911a09c1edc85
hp <shivamsharma220827@acropolis.in>
2026-09-16 16:48:38 +0530

    dp1

---
+++ DailyStreek/numberOfSets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfSets(int n, int k) {
+
+        long MOD = 1000000007L;
+
+        long[] dp = new long[2 * k + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= n + k - 1; i++) {
+
+            for (int j = Math.min(i, 2 * k); j >= 1; j--) {
+                dp[j] = (dp[j] + dp[j - 1]) % MOD;
+            }
+        }
+
+        return (int) dp[2 * k];
+    }
+}
