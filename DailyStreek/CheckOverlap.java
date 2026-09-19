3fb67de60262716edcf0d078c23d1cc99b018183
hp <shivamsharma220827@acropolis.in>
2026-09-19 20:12:13 +0530

    fial

---
+++ DailyStreek/CheckOverlap.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkOverlap(int radius, int xCenter, int yCenter,
+                                int x1, int y1, int x2, int y2) {
+
+        // Circle center ke nearest x-coordinate
+        int closestX = Math.max(x1, Math.min(xCenter, x2));
+
+        // Circle center ke nearest y-coordinate
+        int closestY = Math.max(y1, Math.min(yCenter, y2));
+
+        // Distance squared
+        int dx = xCenter - closestX;
+        int dy = yCenter - closestY;
+
+        return dx * dx + dy * dy <= radius * radius;
+    }
+}
