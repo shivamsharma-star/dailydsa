ffb2bbfcb880e1d6d3640bf2dcfa39d037a4cf73
hp <shivamsharma220827@acropolis.in>
2026-09-26 20:44:22 +0530

    submitted

---
+++ DailyStreek/evaluate.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String evaluate(String s, List<List<String>> knowledge) {
+        
+        // Store key -> value
+        Map<String, String> map = new HashMap<>();
+        
+        for (List<String> pair : knowledge) {
+            map.put(pair.get(0), pair.get(1));
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        int i = 0;
+
+        while (i < s.length()) {
+
+            if (s.charAt(i) == '(') {
+                int j = i + 1;
+
+                // Find closing bracket
+                while (s.charAt(j) != ')') {
+                    j++;
+                }
+
+                // Extract key
+                String key = s.substring(i + 1, j);
+
+                // Replace with value or ?
+                ans.append(map.getOrDefault(key, "?"));
+
+                i = j + 1;
+            } 
+            else {
+                ans.append(s.charAt(i));
+                i++;
+            }
+        }
+
+        return ans.toString();
+    }
+}
